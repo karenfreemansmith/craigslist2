7cbe96b59127fc0cbc341905316ca2d0acc98161
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-01 17:20:50 -0700

    setup files

---
+++ tests/unit/helpers/category-filter-test.js
@@ -0,0 +1,12 @@
+
+import { categoryFilter } from 'craigslist3/helpers/category-filter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | category filter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = categoryFilter([42]);
+  assert.ok(result);
+});
+
